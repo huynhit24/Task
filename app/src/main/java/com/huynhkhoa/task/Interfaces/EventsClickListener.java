53420c6efb38a80343a42525ef18a62b4807f0be
huynhit24 <huynh.it.24@gmail.com>
2022-12-25 19:54:25 +0700

    update: model Task

--- app/src/main/java/com/huynhkhoa/task/Interfaces/EventsClickListener.java
+++ app/src/main/java/com/huynhkhoa/task/Interfaces/EventsClickListener.java
@@ -1,10 +1,12 @@
 package com.huynhkhoa.task.Interfaces;
 
 public interface EventsClickListener {
+    /**
+     * Khai báo các event Click sẽ được triển khai sử dụng
+     * */
+
     void onItemClick(int position);
-
     void onLongItemClick(int position);
-
     void onEditButtonClick(int position);
     void onDeleteButtonClick(int position);
     void onDoneButtonClick(int position);
--- app/src/main/java/com/huynhkhoa/task/Models/TaskModel.java
+++ app/src/main/java/com/huynhkhoa/task/Models/TaskModel.java
@@ -1,12 +1,14 @@
 package com.huynhkhoa.task.Models;
 
+import java.util.Date;
+
 public class TaskModel {
-    private String id, title, description;
-    public TaskModel(String id, String title, String description) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-    }
+
+    private String id;
+
+    private String title;
+
+    private String description;
 
     public String getId() {
         return id;
@@ -19,4 +21,37 @@ public class TaskModel {
     public String getDescription() {
         return description;
     }
+
+    private Date createdAt;
+
+    private Date duedateAt;
+
+    private String priority;
+
+    private Boolean finished;
+
+    public Date getCreatedAt() { return createdAt; }
+
+    public Date getDuedateAt() { return duedateAt; }
+
+    public String getPriority() { return priority; }
+
+    public Boolean getFinished() { return finished; }
+
+    public TaskModel(String id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public TaskModel(String id, String title, String description, Date createdAt, Date duedateAt, String priority, Boolean finished) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.duedateAt = duedateAt;
+        this.priority = priority;
+        this.finished = finished;
+    }
+
 }
