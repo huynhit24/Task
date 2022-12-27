a2530fd1c458cd769c652cf3b839da8e416c0658
huynhit24 <huynh.it.24@gmail.com>
2022-12-27 11:44:54 +0700

    feat: update userinfo

--- app/src/main/java/com/huynhkhoa/task/MainActivity.java
+++ app/src/main/java/com/huynhkhoa/task/MainActivity.java
@@ -2,19 +2,24 @@ package com.huynhkhoa.task;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 //import android.os.PersistableBundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,13 +53,16 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private TextView user_name, user_email;
     private CircleImageView userImage;
-
+    private TextView user_phone, user_profile;
+    String token, uid;
+    Bundle saveStatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sharedPreferenceClass = new SharedPreferenceClass(this);
-
+        token = sharedPreferenceClass.getValue_string("token");
+        saveStatus = savedInstanceState;
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigationView = (NavigationView) findViewById(R.id.navigationView);
         toolbar = findViewById(R.id.toolbar);
@@ -65,6 +73,8 @@ public class MainActivity extends AppCompatActivity {
         user_email = (TextView) hdView.findViewById(R.id.user_email);
         userImage = (CircleImageView) hdView.findViewById(R.id.avatar);
 
+        user_phone =  (TextView) hdView.findViewById(R.id.user_phone);
+        user_profile = (TextView) hdView.findViewById(R.id.user_profile);
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -96,6 +106,10 @@ public class MainActivity extends AppCompatActivity {
                         JSONObject userObj = response.getJSONObject("user");
                         user_name.setText(userObj.getString("username"));
                         user_email.setText(userObj.getString("email"));
+
+                        user_phone.setText(userObj.getString("phone"));
+                        user_profile.setText(userObj.getString("profile"));
+                        uid = userObj.getString("_id");
 
                         Picasso.with(getApplicationContext())
                                 .load(userObj.getString("avatar"))
@@ -173,6 +187,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_home:
                 getSupportFragmentManager().beginTransaction().replace(R.id.content, new HomeFragment()).commit();
                 break;
+            case R.id.action_update_userinfo:
+                onEditButtonClick(uid);
+                break;
             case R.id.action_logout:
                 sharedPreferenceClass.clear();
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
@@ -209,5 +226,98 @@ public class MainActivity extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Show hộp thoại cập nhật Userinfo qua Id
+     * Có chức năng tắt hộp thoại cập nhật
+     * */
+    public void showUpdateDialog(final  String  id, String phone, String profile)  {
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.custom_dialog_layout_update_userinfo, null);
+
+        final EditText phone_field = alertLayout.findViewById(R.id.phone);
+        final EditText profile_field = alertLayout.findViewById(R.id.profile);
+
+        phone_field.setText(phone);
+        profile_field.setText(profile);
+
+        final AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setView(alertLayout)
+                .setTitle("Cập nhật thông tin cá nhân")
+                .setPositiveButton("Cập nhật", null)
+                .setNegativeButton("Hủy bỏ", null)
+                .create();
+
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                Button button = ((AlertDialog)alertDialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String phone = phone_field.getText().toString();
+                        String profile = profile_field.getText().toString();
+
+                        updateUserInfo(id, phone, profile);
+                        finish();
+                        startActivity(getIntent());
+//                        getUserProfile();
+//                        onRestart();
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.content, new HomeFragment()).commit();
+                        alertDialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        alertDialog.show();
+    }
+
+    /**
+     * Cập nhật 1 Task theo Id
+     * Gọi API Put để cập nhật Task
+     * */
+    private  void  updateUserInfo(String id, String phone, String profile) {
+        String url = Constants.BASE_URL + "/api/task/auth/"+id;
+        HashMap<String, String> body = new HashMap<>();
+        body.put("phone", phone);
+        body.put("profile", profile);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url, new JSONObject(body),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if(response.getBoolean("success")) {
+                                Toast.makeText(MainActivity.this, response.getString("msg"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Content-Type", "application/json");
+                params.put("Authorization", token);
+                return params;
+            }
+        };
+
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(10000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    public void onEditButtonClick(String uid) {
+        showUpdateDialog(uid, user_phone.getText().toString(), user_profile.getText().toString());
     }
 }
